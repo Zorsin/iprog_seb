65526840b2322b27ff212f4a9150e36a5eac806f
zorsin <zorsinthedeath@gmail.com>
2016-11-22 11:55:18 +0100

    Loesungen Aufgaben 6 Kommentrare hinzugefuegt

--- src/aufgaben6/Matrix.java
+++ src/aufgaben6/Matrix.java
@@ -44,7 +44,7 @@ class Matrix {
 		
 		/**
 		 * Multipliziert die Matrix mit sich selbst.
-		 * Speichert diese in Ausgabe und gibt dies Vor dem naechsten Durchlauf aus
+		 * Speichert diese in ausgabe und gibt, diese vor dem naechsten Durchlauf aus
 		 */
 		Matrix ausgabe = testMatrix;
 		int n = 1;//Laufvariable
@@ -52,61 +52,62 @@ class Matrix {
 			System.out.println("Ausgabe "+n+":\n"+ausgabe.toString());
 			ausgabe = ausgabe.multiply(testMatrix);
 			n++;
-		}while(n <= 20);
+		}while(n <= 20);//Bis M^20
 		
 		
 		
 	}
 	
-	public Matrix(int m, int n){
-		this.m = m;
+	public Matrix(int m, int n){//Konstruktor der Klasse Matrix
+		this.m = m;//Speichern der uebergabe Parameter in Globalevariablen
 		this.n = n;
 		
-		matrix = new double[this.m][this.n];
+		matrix = new double[this.m][this.n];//erstellt eine neues 2dimensionales double-Array
 	}
 	
-	public Matrix multiply(Matrix mat){
-		Matrix out = new Matrix(this.m,this.n);
+	public Matrix multiply(Matrix mat){//Berechnung fuer die Multiplikation
+		Matrix out = new Matrix(this.m,this.n);//Erstellen der Ausgabe Matrix
 		
-		if(!(this.n == mat.m)){
-			return new Matrix(0,0);
+		if(!(this.n == mat.m)){//Da bei der Multiplikation die Zeilen der einen mit den Spalten der anderen gleichsein muessen
+			return new Matrix(0,0);//Wenn das nicht der Fall return einer leern Matrix der groesse 0
 		}
-		for(int i =1; i <= this.m; i++){
+		for(int i =1; i <= this.m; i++){//For Schreifen fuer jedes Element einer Matrix !! Wichtig Laufvariablen auf getEntry und setEntry anpassen (Zaehlweise)
 			for(int j = 1; j <= this.n; j++){
-				out.setEntry(i, j, this.getEntry(i,j) * mat.getEntry(j, i));
+				out.setEntry(i, j, this.getEntry(i,j) * mat.getEntry(j, i));//Speichert die Multiplikaiton in der Matrix out
 			}
 		}
 		
 		return out;
 	}
 	
-	public Matrix init(double wert){
+	public Matrix init(double wert){//Fuellt die Matrix mit einem Wert, wird nicht fuer die Aufgaben benoetigt, war zur Kontrolle aber hilfreich
 		Matrix out = new Matrix(this.m,this.n);
 		
-		for(int i =1; i <= this.m; i++){
+		for(int i =1; i <= this.m; i++){//For-Schleifen fuer jedes Element !! Wichtig Laufvariablen auf getEntry und setEntry anpassen (Zaehlweise)
 			for(int j = 1; j <= this.n; j++){
-				out.setEntry(i, j, wert);
+				out.setEntry(i, j, wert);//Fuellt jede Position mit die Wert wert
 			}
 		}
 		
 		return out;
 	}
 	
-	public Matrix add(Matrix mat){
-		Matrix out = new Matrix(this.m,this.n);
-		if(!(this.n == mat.n) || !(this.m == mat.m)){
-			return new Matrix(0,0);
+	public Matrix add(Matrix mat){//Methode zur Addition einer Matrix
+		Matrix out = new Matrix(this.m,this.n);//Erstellt neue Matrix
+		
+		if(!(this.n == mat.n) || !(this.m == mat.m)){//Bei der Addition muessen die Matrizen gleich gross sein
+			return new Matrix(0,0);//Wenn es nicht zutrifft dann leer-Matrix zurueck
 		}
-		for(int i = 1; i <= this.m; i++){
+		for(int i = 1; i <= this.m; i++){//For-Schleifen fuer jedes Element !! Wichtig Laufvariablen auf getEntry und setEntry anpassen (Zaehlweise)
 			for(int j = 1; j <= this.n; j++){
-				out.setEntry(i, j, this.getEntry(i, j) + mat.getEntry(i, j));
+				out.setEntry(i, j, this.getEntry(i, j) + mat.getEntry(i, j));//Addition der Matrizen und Speicherung in out
 			}
 		}
 		
 		return out;
 	}
 	
-	public boolean checkFilled(){
+	public boolean checkFilled(){//Methode zur Pruefung ob Matrix leer, wird nicht in der Aufgabe verlangt, kann man zur Pruefung ob Add oder Multiply erfolgreich nutzen
 		if(!(this.n == 0) || !(this.m == 0)){
 			return true;
 		}else{
@@ -115,18 +116,23 @@ class Matrix {
 	}
 	
 	
-	public void setEntry(int i, int j, double x){
-		this.matrix[i-1][j-1] = x;
+	public void setEntry(int i, int j, double x){//Methode zum Setzen eines Eintrages an die Position i,j
+		this.matrix[i-1][j-1] = x;//Da eine "normaler" Mensch bei 1 anfaengt zu zaehlen muss die Position um 1 verschoben werden
 	}
 	
-	public String toString(){
+	public String toString(){//Methode fuer String-Rueckgabe
 		String out = "";
+		//Wenn es nicht moeglich, weil Matrix leer 
 		try{
-			for(double[] element : matrix){
+			/**
+			 * Hier habe ich foreach Schleifen genutzt, da ich nur eine Matrix habe und ich die Position nicht 
+			 * benoetige wie bei der Addition oder Multiplikation. Man kann auch hier die normale for-Schleife nutzen 
+			 */
+			for(double[] element : matrix){//Foreach-Schleifen fuer jedes Element 
 				for(double zahl : element){
-					out += " "+zahl;
+					out += " "+zahl;//fuegt die Zahlen einer Zeile durch Leerzeichen getrennt aneinander
 				}
-				out += "\n";
+				out += "\n";//fuegt nach jeder Zeile einen Zeilenumbruch hinzu
 			}
 		}catch(Exception ex){
 			return "Die Matrix ist leer (null)";
@@ -135,8 +141,8 @@ class Matrix {
 		return out;
 	}
 	
-	public double getEntry(int i, int j){
-		return matrix[i-1][j-1];
+	public double getEntry(int i, int j){//Methode um einen Eintrag an der Position i,j zu erhalten
+		return matrix[i-1][j-1];//Da eine "normaler" Mensch bei 1 anfaengt zu zaehlen muss die Position um 1 verschoben werden
 	}
 
 }
--- src/aufgaben6/TrhekegmuLister.java
+++ src/aufgaben6/TrhekegmuLister.java
@@ -14,37 +14,40 @@ class TrhekegmuLister {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		InteractivIO mIO = new InteractivIO();
-		ArrayList<String> listString = new ArrayList<String>();
-		boolean running = true;
-		do{
-			String entry = mIO.promtAndRead("Bitte ein Wort angeben:");
-			if(entry == null || entry.equals("guneg")  || entry.equals("^Z")){
-				running = false;
-			}else{
-				listString.add(entry);
-			}
-			
-		}while(running);
-		int i = listString.size();
-		String[] arrayString = new String[i];
-		i--;
+	public static void main(String[] args) {//Main-Methode
+		InteractivIO mIO = new InteractivIO();//Initialisieren und Deklarieren der InteractivIO-Klasse
 		
-		for(String element : listString){
+		ArrayList<String> listString = new ArrayList<String>();//Erzeugen einer ArrayList zum Abspeichern der Strings
+		boolean running = true;//Laufvariable fuer Schleife
+		/**
+		 * Die Schleife wird solange ausgefuehrt bis eine Abbruchbedinung zu trifft
+		 */
+		do{
+			String entry = mIO.promtAndRead("Bitte ein Wort angeben:");//Einlesen des Strings
+			if(entry == null || entry.equals("guneg")  || entry.equals("^Z")){//Wenn das eingelesene Wort einer Abbruchbedinung gleich ist
+				running = false;//Schleife beenden
+			}else{
+				listString.add(entry);//Wort der Liste hinzufuegen
+			}
+		}while(running);
+		
+		int i = listString.size();//Groesse der Liste als Laufvariable speichern
+		String[] arrayString = new String[i];//Array der Groesse i erzeugen
+		i--;//Variable verringern, da Array bei 0 anfaengt zu zaehlen
+		
+		for(String element : listString){//foreach Schleife fuer Umwandlung Liste in Array
 			
-			arrayString[i] = invert(element);
+			arrayString[i] = invert(element);//Speichert das Eingelesene Wort invertiert. Das Array wird von hinten nach vorne gefuellt
 			i--;
-			
 		}
 		
-		for(String element : arrayString){
+		for(String element : arrayString){//Foreach Schleife zur Ausgabe des Arrays
 			mIO.write(element);
 		}
 
 	}
 	
-	private static String invert(String text){
+	private static String invert(String text){//Invert-Methode aus den Aufgaben 1 (zipinvert)
 		return text.length() <= 1 ? text : invert(text.substring(1))+text.substring(0,1);
 	}
 
